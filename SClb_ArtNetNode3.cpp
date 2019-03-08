6044b242ce6d31fa252831b516ba62b48a660a2c
Eugene Mikhaylov <statuscue@hes.od.ua>
2019-03-08 20:02:07 +0200

    Add project files.

---
+++ SClb_ArtNetNode3.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		SClb_ArtNetNode3.cpp
+ Created:	08-Mar-19 20:02:03
+ Author:	statu
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "SClb_ArtNetNode3.h"
+
+
---
+++ SClb_ArtNetNode3.h
@@ -0,0 +1,19 @@
+/*
+ Name:		SClb_ArtNetNode3.h
+ Created:	08-Mar-19 20:02:03
+ Author:	statu
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _SClb_ArtNetNode3_h
+#define _SClb_ArtNetNode3_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
